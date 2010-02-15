8ec372edef2b8551f881ff9267c6f8870f91a586
klondike <kprojection@gmail.com>
2010-02-15 04:07:52 -0500

    Looking up accounts by user

--- ohnomymoney.rb
+++ ohnomymoney.rb
@@ -4,6 +4,17 @@ require 'rubygems'
 require 'sinatra'
 require 'environment'
 
-get '/' do
-  Account.all.map(&:name).join("<br/>\n")
+# For now, have the root URL act as if we visited the userpage of the first user
+before do
+  request.path_info = "/#{User.first.handle}" if request.path_info == '/'
+end
+
+get "/:handle" do
+  halt 404 unless user = User.find_by_handle(params[:handle])
+  
+  user.accounts.all.map(&:name).join("<br/>\n")
+end
+
+not_found do
+  "User not found"
 end
