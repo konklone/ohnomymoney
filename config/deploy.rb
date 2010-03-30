ab54b3d16df9a3bb1b1ee080208a8655843e7e89
klondike <kprojection@gmail.com>
2010-03-29 22:35:47 -0400

    Set up Capistrano for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+set :environment, :production
+
+set :user, 'klondike'
+set :application, 'ohnomymoney'
+
+set :deploy_to, "/home/#{user}/ohnomymoney.com"
+set :domain, 'ohnomymoney.com'
+
+set :scm, :git
+set :repository, "git://github.com/klondike/#{application}.git"
+set :branch, 'master'
+
+set :deploy_via, :remote_cache
+set :runner, user
+set :admin_runner, runner
+
+role :app, domain
+role :web, domain
+
+set :use_sudo, false
+after "deploy", "deploy:cleanup"
+after "deploy:update_code", "deploy:shared_links"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :migrate do; end
+  
+  desc "Restart the server"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join current_path, 'tmp', 'restart.txt'}"
+  end
+  
+  desc "Get shared files into position"
+  task :shared_links, :roles => [:web, :app] do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/buxfer.yml #{release_path}/updater/buxfer.yml"
+    run "ln -nfs #{shared_path}/dreamhost.rb #{release_path}/dreamhost.rb"
+    run "rm #{File.join release_path, 'tmp', 'pids'}"
+    run "rm #{File.join release_path, 'public', 'system'}"
+    run "rm #{File.join release_path, 'log'}"
+  end
+end
